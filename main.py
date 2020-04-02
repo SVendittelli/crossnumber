7704fdac599b50decbb47c311fd0a6f52f6451ad
Sam Vendittelli <sam.vendittelli@hotmail.com>
2020-04-02 21:50:41 +0100

    create basic flask application

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def hello_name(name='World'):
+    return { 'message': get_message(name) }
+
+def get_message(name):
+    return 'Hello, {}!'.format(escape(name))
