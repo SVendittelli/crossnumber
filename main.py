9719c4981ffab576cbcb2817ad89e99a9289f86f
Sam Vendittelli <sam.vendittelli@hotmail.com>
2020-04-05 14:57:42 +0100

    Say hello to Rosa

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from flask import Flask
 from markupsafe import escape
 app = Flask(__name__)
 
+def hello_name(name='World'):
+    return { 'message': get_message(name) }
+
 @app.route('/')
 @app.route('/<name>')
-def hello_name(name='World'):
+def better_name(name='Team!'):
+    return { 'message': get_message(name) }
+
+def greet_rich(name='Rich :)'):
     return { 'message': get_message(name) }
 
 def get_message(name):
     return 'Hello, {}!'.format(escape(name))
 
 def function_for_sam2():
-    print("hey Sam :)") 
+    print("hey Sam :)")
+    print('Hey Rosa')
