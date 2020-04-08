103c24ee8d9ae7b558e1ad20412c70de1d55b1e7
Richard Lewis <rich-27@users.noreply.github.com>
2020-04-08 18:02:50 +0100

    Re-did move to collapsible panel without pulling in material

--- frontend/barebones-frontend/src/app/app.component.ts
+++ frontend/barebones-frontend/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'barebones-frontend';
-  defaultContentOpenState = true;
+  defaultContentCollapsedState = true;
 }
--- frontend/barebones-frontend/src/app/app.module.ts
+++ frontend/barebones-frontend/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,9 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatExpansionModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
